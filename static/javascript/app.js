86599a4d5def170224866f52710b5f27850b7850
dyl-lee <dylanlee1701@gmail.com>
2022-01-12 23:48:58 -0500

    built table function

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -3,3 +3,16 @@ const tableData = data;
 
 // reference HTML table using d3
 var tbody = d3.select("tbody");
+
+// build table function
+function buildTable(data) {
+    tbody.html("");                                     // tbody.html points JS to table in index.html, ("") represents empty string. Therefore, use empty string when creating table
+    data.forEach((dataRow) => {                         // forEach() only loops through arrays...
+        let row = tbody.append("tr");                   // adds data row by row, JS: find tbody in HTML and add row ("tr"). Keep in mind this code will search for <tr> tag. Here, row is limited to just this code block therefore let instead of var.
+        Object.values(dataRow).forEach((val) => {       // each object is an individual sighting. Add values() into dataRow, forEach((val) specifies one object per row, where val = each item in object
+            let cell = row.append("td");                // append each object value to a cell/table data <td> tag in table
+            cell.text(val);                             // chain .text(value) to the variable, cell. Extracts text of value to add data to cell
+            }
+        );
+    });
+}
